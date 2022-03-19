5743931162783db1b11c30f9a4538b20ee0ea1ec
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-18 21:03:47 -0400

    chore: relocates to p6m7g8-dotfiles (#1)

--- init.zsh
+++ init.zsh
@@ -7,7 +7,7 @@
 ######################################################################
 p6df::modules::sudo::deps() {
     ModuleDeps=(
-        p6m7g8/p6common
+        p6m7g8-dotfiles/p6common
         ohmyzsh/ohmyzsh:plugins/sudo
     )
 }
