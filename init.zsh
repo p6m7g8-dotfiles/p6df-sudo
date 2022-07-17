2bd36a21e98ba0a6e3a4eee67cbfa43fcd9dbde1
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-07-17 14:05:05 -0400

    fix: adds p6df-zsh for compdef

--- init.zsh
+++ init.zsh
@@ -7,7 +7,7 @@
 ######################################################################
 p6df::modules::sudo::deps() {
     ModuleDeps=(
-        p6m7g8-dotfiles/p6common
+        p6m7g8-dotfiles/p6df-zsh
         ohmyzsh/ohmyzsh:plugins/sudo
     )
 }
